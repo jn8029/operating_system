a1e93285ba227e5900894ed97208ee1d8916ec4e
“Warren_Cheng” <warren.y.cheng@gmail.com>
2019-03-26 15:13:11 +0800

    finish 322

--- Processes/ch3_322.c
+++ Processes/ch3_322.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <stdlib.h> /*strtol*/
+#include <unistd.h> /*fork*/
+#include <sys/wait.h> /*wait*/
+#include <sys/mman.h> /*shm_open, shm_unlink*/
+#include <fcntl.h> /*O_RDWR, O_RDONLY, O_CREAT*/
+#include <string.h>
 
 #define SIZE 4096
+
 char* stringify(unsigned long long int number){
-  printf("instringigy %llu\n",number);
-  char char_array[1024];
-  sprintf(char_array, "%llu", number);
+  char char_array[SIZE];
+  sprintf(char_array, "%llu ", number);
   char *char_ptr;
   char_ptr = &char_array[0];
   return char_ptr;
 }
 void execute_algo(char* arg, void *write_ptr){
-  printf("executing algo....%s\n",arg);
+
   char *buffer;
   unsigned long long int converted_int = strtol(arg, &buffer, 10);
-  printf("start here 123....%llu\n", converted_int);
+
+  sprintf(write_ptr, "%s", arg);
+  write_ptr += strlen(arg);
+
   if (converted_int == 0){
     printf("input not pos int\n");
     return;
   }
-  printf("start here....345%llu\n", converted_int);
+
   while (converted_int > 1){
 
     if (converted_int % 2 == 0){
@@ -35,68 +36,55 @@ void execute_algo(char* arg, void *write_ptr){
       converted_int = 3 * converted_int +1;
     }
     char* stringified_int = stringify(converted_int);
-    sprintf(write_ptr, "%s\t", stringified_int);
-    write_ptr += sizeof(stringified_int);
+    sprintf(write_ptr, " %s", stringified_int);
+    /*sizeof a char* is 8 bytes, strlen iterates thru the array until reach \0*/
+    write_ptr += strlen(stringified_int);
   }
-  printf("holalalalal\n");
+
 }
 
-void create_read_shared_memory(const char* name, void **ptr){
-	int shm_fd;
+void* create_readonly_shared_memory(const char* name){
+  void *ptr;
+  int shm_fd;
 	shm_fd = shm_open(name, O_CREAT | O_RDONLY, 0666);
-  printf("read_shared memory fd = %d\n",shm_fd);
+
 	ftruncate(shm_fd, SIZE);
-	*ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
-  printf("read shared mem created\n");
+	ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+
+  return ptr;
 }
 
-void access_write_shared_memory(const char* name, void **ptr){
-	int shm_fd;
-	shm_fd = shm_open(name , O_RDWR, 0666);
-  printf("write_shared memory fd = %d\n",shm_fd);
-	*ptr =mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd,0);
-
-  printf("write shared mem created\n");
+void* access_write_shared_memory(const char* name){
+  void *write_ptr;
+  int shm_fd;
+  shm_fd = shm_open(name , O_RDWR, 0666);
+  write_ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd,0);
+  return write_ptr;
 }
 int main(int argc, char* argv[]){
   const char* shm_name = "OS";
-
   void *ptr;
-  int shm_fd;
-	shm_fd = shm_open(shm_name, O_CREAT | O_RDONLY, 0666);
-  printf("read_shared memory fd = %d\n",shm_fd);
-	ftruncate(shm_fd, SIZE);
-	ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
-  printf("read shared mem created\n");
+  ptr = create_readonly_shared_memory(shm_name);
   pid_t pid = fork();
-  printf("child process created %d\n",pid);
-
   if (pid == 0){
-
     if (argc == 2){
-      void *write_ptr;
-      int shm_fd_w;
-    	shm_fd_w = shm_open(shm_name , O_RDWR, 0666);
-      printf("write_shared memory fd = %d\n",shm_fd_w);
-    	write_ptr =mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd_w,0);
-      printf("write shared mem created\n");
-      char *message = "fuck";
-      sprintf(write_ptr,"%s",message);
-      // execute_algo(argv[1], write_ptr);
+      void* write_ptr;
+      write_ptr = access_write_shared_memory(shm_name);
+      execute_algo(argv[1], write_ptr);
     } else {
       printf("Error:incorrect argc\n");
     }
-    printf("last lne of child\n");
+
     return 0;
 
-  }else if (pid<0){
-    printf("fork failed\n");
+  } else if (pid<0){
+    printf("Error: fork failed\n");
   } else {
-    int *status;
     pid_t finished_child;
-    finished_child = wait(status);
-    printf("child %d is done..with status code %d\n", finished_child, *status);
-    printf("message received %s", (char *)ptr);
+    finished_child = wait(NULL);
+    printf("Child pid = %d is done.\n", finished_child);
+    printf("Message received is...\n");
+    printf("%s\n", (char *)ptr);
     shm_unlink(shm_name);
 
   }
