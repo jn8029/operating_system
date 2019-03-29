37b8ea71a7981be69eea4f474a826ada9ae84a86
jn8029 <warren.y.cheng@gmail.com>
2019-03-29 00:11:31 -0600

    add module task display

---
+++ test.c
@@ -0,0 +1,28 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+
+/* This function is called when the module is loaded. */
+int simple_init(void)
+{
+	struct task_struct *task;
+	for_each_process(task){
+		printk("%s, pid= %d state= %d\n",task->comm, task->pid, task->state);
+	}
+	return 0;
+}
+
+/* This function is called when the module is removed. */
+void simple_exit(void) {
+	printk(KERN_INFO "Removing Module\n");
+}
+
+/* Macros for registering module entry and exit points. */
+module_init( simple_init );
+module_exit( simple_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
+
